4874d9a5b04b4f9c310f5c9dea856608fcd39e92
Nikita Bezhenar <kross78080@yandex.ru>
2019-08-02 01:09:51 +0300

    dispatch parses formats

--- src/parses.js
+++ src/parses.js
@@ -1,21 +1,11 @@
-import fs from 'fs';
-import path from 'path';
 import yaml from 'js-yaml';
 import ini from 'ini';
 
-const getParseFormat = (format) => {
+export default (format) => {
   const formatParsers = {
     '.json': JSON.parse,
     '.yml': yaml.safeLoad,
     '.ini': ini.parse,
   };
   return formatParsers[format];
-};
-
-
-export default (filepath) => {
-  const file = fs.readFileSync(path.resolve('__tests__/__fixtures__/', filepath), 'utf-8');
-  const format = path.extname(filepath);
-  const parser = getParseFormat(format);
-  return parser(file);
 };
